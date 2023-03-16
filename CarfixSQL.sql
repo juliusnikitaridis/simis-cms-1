9126a1cbb21acd2c02025242e193171eabd28b2b
Julius Nikitaridis <julius@connectmobiles24.com>
2023-03-16 13:39:09 +0200

    update created tables SQL

--- CarfixSQL.sql
+++ CarfixSQL.sql
@@ -1,124 +1,149 @@
 create table carfix.categories
 (
-    id          varchar,
+    id varchar,
     description varchar,
-    category    varchar
+    category varchar
 );
 
-alter table carfix.categories
-    owner to postgres;
+alter table carfix.categories owner to postgres;
 
 create table carfix.brands
 (
-    id         varchar,
+    id varchar,
     brand_name varchar
 );
 
-alter table carfix.brands
-    owner to postgres;
+alter table carfix.brands owner to postgres;
 
 create table carfix.vehicles
 (
-    id               varchar,
-    vin_number       varchar,
-    registration     varchar,
-    make             varchar,
-    model            varchar,
-    year             varchar,
-    fuel_type        varchar,
-    transmission     varchar,
-    odo_reading      varchar,
-    engine_code      varchar,
-    member_id        varchar,
-    service_history  varchar,
-    maintenance_plan varchar
+    id varchar,
+    vin_number varchar,
+    registration varchar,
+    make varchar,
+    model varchar,
+    year varchar,
+    fuel_type varchar,
+    transmission varchar,
+    odo_reading varchar,
+    engine_code varchar,
+    member_id varchar,
+    service_history varchar,
+    maintenance_plan varchar,
+    make_id varchar
 );
 
-alter table carfix.vehicles
-    owner to postgres;
+alter table carfix.vehicles owner to postgres;
 
 create table carfix.service_request
 (
-    id                            varchar(255),
-    created_date                  varchar(255),
-    type                          varchar(255),
-    vehicle_id                    varchar(255),
-    member_id                     varchar(255),
-    radius                        varchar(255),
-    status                        varchar(255),
-    current_odo_reading           varchar(255),
-    picture_data                  varchar,
-    last_service_date             varchar(255),
-    additional_description        varchar,
-    accepted_quote_id             varchar,
+    id varchar(255),
+    created_date varchar(255),
+    type varchar(255),
+    vehicle_id varchar(255),
+    member_id varchar(255),
+    radius varchar(255),
+    status varchar(255),
+    current_odo_reading varchar(255),
+    picture_data varchar,
+    last_service_date varchar(255),
+    additional_description varchar,
+    accepted_quote_id varchar,
     confirmed_service_provider_id varchar,
-    category_hash                 varchar,
-    vehicle_brand_id              varchar,
-    preferred_date                varchar,
-    confirmed_date                varchar
+    category_hash varchar,
+    vehicle_brand_id varchar,
+    preferred_date varchar,
+    confirmed_date varchar,
+    job_number varchar
 );
 
-alter table carfix.service_request
-    owner to postgres;
+alter table carfix.service_request owner to postgres;
 
 create table carfix.quote
 (
-    id                     varchar,
+    id varchar,
     request_for_service_id varchar,
-    service_provider_id    varchar,
-    booking_date           varchar,
-    total                  varchar,
-    service_provider_name  varchar,
-    status                 varchar,
-    quantity               varchar,
-    created_date           varchar
+    service_provider_id varchar,
+    booking_date varchar,
+    total varchar,
+    service_provider_name varchar,
+    status varchar,
+    quantity varchar,
+    created_date varchar
 );
 
-alter table carfix.quote
-    owner to postgres;
+alter table carfix.quote owner to postgres;
 
 create table carfix.quotation_item
 (
-    id               varchar,
-    quote_id         varchar,
-    part_number      varchar,
+    id varchar,
+    quote_id varchar,
+    part_number varchar,
     part_description varchar,
     item_total_price varchar,
-    quantity         varchar
+    quantity varchar,
+    item_type varchar,
+    item_status varchar,
+    labour_total varchar,
+    parts_reason varchar,
+    replacement_reason varchar,
+    parts_total varchar,
+    parts_picture varchar
 );
 
-alter table carfix.quotation_item
-    owner to postgres;
+alter table carfix.quotation_item owner to postgres;
 
 create table carfix.service_provider
 (
-    id                   varchar,
-    supported_brands     varchar,
-    name                 varchar,
-    services             varchar,
-    certifications       varchar,
-    user_id              varchar,
-
-    address              varchar,
-    about_us             varchar,
-    logo_data            varchar,
+    id varchar,
+    supported_brands varchar,
+    name varchar,
+    services varchar,
+    certifications varchar,
+    user_id varchar
+        constraint service_provider_users_unique_id_fk
+            references users (unique_id),
+    address varchar,
+    about_us varchar,
+    logo_data varchar,
     supported_categories varchar,
-    accreditations       varchar,
-    rating               integer,
-    count                integer
+    accreditations varchar,
+    rating integer,
+    count integer,
+    drop_off varchar,
+    rmi varchar
 );
 
-alter table carfix.service_provider
-    owner to postgres;
+alter table carfix.service_provider owner to postgres;
 
 create table carfix.service_request_items
 (
-    id                 varchar,
+    id varchar,
     service_request_id varchar,
-    category_id        varchar,
-    item_description   varchar
+    category_id varchar,
+    item_description varchar
 );
 
-alter table carfix.service_request_items
-    owner to postgres;
+alter table carfix.service_request_items owner to postgres;
+
+create table carfix.payment_history
+(
+    amount varchar,
+    merchant_transaction_no varchar,
+    member_id varchar,
+    service_provider_id varchar,
+    id varchar,
+    status varchar,
+    raw_request varchar
+);
+
+alter table carfix.payment_history owner to postgres;
+
+create table carfix.device_token
+(
+    unique_id varchar,
+    token varchar
+);
+
+alter table carfix.device_token owner to postgres;
 
