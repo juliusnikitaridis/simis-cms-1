96c67030da12b227376136b565375e4cc0d9e8c8
Julius Nikitaridis <julius@connectmobiles24.com>
2023-08-03 10:41:56 +0200

    readded CreateComplianceUserService, UpdateYieldService

---
+++ src/main/java/com/simisinc/platform/rest/services/cannacomply/UpdateYieldService.java
@@ -0,0 +1,48 @@
+package com.simisinc.platform.rest.services.cannacomply;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simisinc.platform.domain.model.cannacomply.Block;
+import com.simisinc.platform.domain.model.carfix.Yield;
+import com.simisinc.platform.infrastructure.persistence.cannacomply.BlockRepository;
+import com.simisinc.platform.infrastructure.persistence.carfix.YieldRepository;
+import com.simisinc.platform.rest.controller.ServiceContext;
+import com.simisinc.platform.rest.controller.ServiceResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+
+
+/**
+ * Description
+ * @author Julius Nikitaridis
+ * @created 04/05/23 11:28 AM
+ */
+
+
+public class UpdateYieldService {
+
+    private static Log LOG = LogFactory.getLog(UpdateYieldService.class);
+
+    public ServiceResponse post(ServiceContext context) {
+
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            Yield item = mapper.readValue(context.getJsonRequest(), Yield.class);
+
+            YieldRepository.update(item);
+
+            ServiceResponse response = new ServiceResponse(200);
+            ArrayList<String> responseMessage = new ArrayList<String>(){{add("Yield has been updated");}};
+            response.setData(responseMessage);
+            return response;
+
+        } catch (Exception e) {
+            LOG.error("Error in UpdateYieldService", e);
+            ServiceResponse response = new ServiceResponse(400);
+            response.getError().put("title", e.getMessage());
+            return response;
+        }
+    }
+}
